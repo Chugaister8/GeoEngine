7bdb036ae63cb30e05c98d9d84dd05b2db173122
Chugaister8 <chugaister9@gmail.com>
2026-05-16 23:46:59 +0300

    Create index.ts

---
+++ packages/core-js/src/utils/index.ts
@@ -0,0 +1,8 @@
+/**
+ * GeoEngine utils — публічний API
+ */
+
+export * from "./math"
+export * from "./pool"
+export { LRUCache } from "./lruCache"
+export type { LRUCacheOptions } from "./lruCache"
