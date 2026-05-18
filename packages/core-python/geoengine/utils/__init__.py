fc052de20fe72056eddfa630db941b24c9eb9557
Chugaister8 <chugaister9@gmail.com>
2026-05-18 17:25:23 +0300

    Update __init__.py

--- packages/core-python/geoengine/utils/__init__.py
+++ packages/core-python/geoengine/utils/__init__.py
@@ -1,23 +1,19 @@
 """GeoEngine — utils пакет."""
 
 from .math3d import (
-    Vec2, Vec3, Vec4, Quat, Mat4,
-    AABB, Ray,
-    lerp, clamp, smoothstep,
-    ease_in_out_cubic,
+    Vec2, Vec3, Vec4, Quat, Mat4, AABB, Ray,
     deg_to_rad, rad_to_deg,
-    angle_wrap_360, angle_wrap_180,
+    clamp, lerp, smoothstep, sign,
+    DEG2RAD, RAD2DEG, EPSILON, PI,
 )
 from .logging import configure_logging, get_logger, log_context
-from .config  import BaseGeoConfig, get_config
+from .config  import BaseGeoConfig
 
 __all__ = [
-    "Vec2", "Vec3", "Vec4", "Quat", "Mat4",
-    "AABB", "Ray",
-    "lerp", "clamp", "smoothstep",
-    "ease_in_out_cubic",
+    "Vec2", "Vec3", "Vec4", "Quat", "Mat4", "AABB", "Ray",
     "deg_to_rad", "rad_to_deg",
-    "angle_wrap_360", "angle_wrap_180",
+    "clamp", "lerp", "smoothstep", "sign",
+    "DEG2RAD", "RAD2DEG", "EPSILON", "PI",
     "configure_logging", "get_logger", "log_context",
-    "BaseGeoConfig", "get_config",
+    "BaseGeoConfig",
 ]
